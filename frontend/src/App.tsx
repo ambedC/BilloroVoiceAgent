bc82d138a9000d32944afe02bcfc43af88df5647
ambedC <ambedcmofficial@gmail.com>
2026-02-16 14:43:25 +0530

    Conflict fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/Layout";
 import Dashboard from "./pages/Dashboard";
 import LiveCalls from "./pages/LiveCalls";
